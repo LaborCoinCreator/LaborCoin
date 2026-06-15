ed3b12fac28866fb3a46ebdca49d9f2a3fbbec07
LaborCoinCreator <laborcoincreator@proton.me>
2026-06-14 18:56:03 -0700

    Update LaborCoinTreasuryModuleV1.sol

--- final-launch/contracts/LaborCoinTreasuryModuleV1.sol
+++ final-launch/contracts/LaborCoinTreasuryModuleV1.sol
@@ -1,1 +1,98 @@
-LaborCoinTreasuryModuleV1
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract LaborCoinTreasuryModuleV1
+{
+    // =====================================================
+    // DAO
+    // =====================================================
+
+    address public immutable DAO;
+
+    uint256 public totalDistributed;
+
+    // =====================================================
+    // EVENTS
+    // =====================================================
+
+    event TransferExecuted(
+        address indexed recipient,
+        uint256 amount
+    );
+
+    // =====================================================
+    // CONSTRUCTOR
+    // =====================================================
+
+    constructor(
+        address _dao
+    ) {
+
+        require(
+            _dao != address(0),
+            "Invalid DAO"
+        );
+
+        DAO = _dao;
+    }
+
+    // =====================================================
+    // ONLY DAO
+    // =====================================================
+
+    modifier onlyDAO() {
+
+        require(
+            msg.sender == DAO,
+            "Only DAO"
+        );
+
+        _;
+    }
+
+    // =====================================================
+    // EXECUTE TRANSFER
+    // =====================================================
+
+    function executeTransfer(
+        address payable recipient
+    )
+        external
+        payable
+        onlyDAO
+    {
+
+        require(
+            recipient != address(0),
+            "Invalid recipient"
+        );
+
+        require(
+            msg.value > 0,
+            "Invalid amount"
+        );
+
+        (bool success,) =
+            recipient.call{
+                value: msg.value
+            }("");
+
+        require(
+            success,
+            "Transfer failed"
+        );
+
+        totalDistributed += msg.value;
+
+        emit TransferExecuted(
+            recipient,
+            msg.value
+        );
+    }
+
+    // =====================================================
+    // RECEIVE
+    // =====================================================
+
+    receive() external payable {}
+}
